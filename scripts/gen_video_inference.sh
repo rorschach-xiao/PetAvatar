622f745de47f19e1ae894d97f37b1d9ac92ce8d1
xiaoyang <xiao.yang3@northeastern.edu>
2024-03-29 16:45:07 -0700

    change animatediff to animatediff_sdxl

--- scripts/gen_video_inference.sh
+++ scripts/gen_video_inference.sh
@@ -9,13 +9,13 @@ echo "==>> Starting generating videos..."
 
 # generate config file
 python src/animation/config_generator.py --lora_weight_path ${LORA_PATH} --prompt_file_path ${PROMPT_FILE} --step ${STEP} --guidance_scale ${GUIDANCE_SCALE} --lora_alpha ${LORA_ALPHA}
-mv ./5-lora.yaml src/animation/AnimateDiff/configs/prompts/
+mv ./5-lora.yaml src/animation/AnimateDiff_sdxl/configs/prompts/
 
 # copy lora weight
-cp ${LORA_PATH} src/animation/AnimateDiff/models/DreamBooth_LoRA/lora.safetensors
+cp ${LORA_PATH} src/animation/AnimateDiff_sdxl/models/DreamBooth_LoRA/lora.safetensors
 
 # generate video
-cd ./src/animation/AnimateDiff
+cd ./src/animation/AnimateDif_sdxl
 CUR_PATH=`pwd`
 python -m scripts.animate --exp_config configs/prompts/5-lora.yaml --H 1024 --W 1024 --L 16 --xformers --output_dir ${CUR_PATH}/../../../sdxl_video/${OUTPUT_DIR}
 
