5647166347c801b1160941783634909bc28b0ebf
xiaoyang <xiao.yang3@northeastern.edu>
2024-03-29 02:27:16 -0700

    inference script update

--- scripts/gen_video_inference.sh
+++ scripts/gen_video_inference.sh
@@ -8,11 +8,11 @@ LORA_ALPHA=$6
 echo "==>> Starting generating videos..."
 
 # generate config file
-cd ./src/animations/
+cd ./src/animation/
 python config_generator.py --lora_weight_path ${LORA_PATH} --prompt_file_path ${PROMPT_FILE} --step ${STEP} --guidance_scale ${GUIDANCE_SCALE} --lora_alpha ${LORA_ALPHA}
 
 # generate video
-cd ./src/animations/AnimateDiff
+cd ./src/animation/AnimateDiff
 CUR_PATH=`pwd`
 python -m scripts.animate --exp_config configs/prompts/5-lora.yaml --H 1024 --W 1024 --L 16 --xformers --output_dir ${CUR_PATH}/../${OUTPUT_DIR}
 
