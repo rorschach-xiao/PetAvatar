7b6dc14398b4cf5dbfefee785766144e426663a6
xiaoyang <xiao.yang3@northeastern.edu>
2024-03-30 15:21:23 -0700

    support sd15 animation

--- scripts/gen_video_inference.sh
+++ scripts/gen_video_inference.sh
@@ -4,22 +4,41 @@ OUTPUT_DIR=$3
 STEP=$4
 GUIDANCE_SCALE=$5
 LORA_ALPHA=$6
+SD_BASE=$7
 
 echo "==>> Starting generating videos..."
 
 # generate config file
-python src/animation/config_generator.py --lora_weight_path ${LORA_PATH} --prompt_file_path ${PROMPT_FILE} --step ${STEP} --guidance_scale ${GUIDANCE_SCALE} --lora_alpha ${LORA_ALPHA}
-mv ./5-lora.yaml src/animation/AnimateDiff_sdxl/configs/prompts/
+if [[ "$SD_BASE" == "sd15" ]]
+then
+  python src/animation/config_generator.py --sd_base ${SD_BASE} --lora_weight_path ${LORA_PATH} --prompt_file_path ${PROMPT_FILE} --step ${STEP} --guidance_scale ${GUIDANCE_SCALE} --lora_alpha ${LORA_ALPHA}
+  mv ./5-lora.yaml src/animation/AnimateDiff_sd15/configs/prompts/
 
-# copy lora weight
-cp ${LORA_PATH} src/animation/AnimateDiff_sdxl/models/DreamBooth_LoRA/lora.safetensors
+  # copy lora weight
+  cp ${LORA_PATH} src/animation/AnimateDiff_sd15/models/DreamBooth_LoRA/lora.safetensors
 
-# generate video
-cd ./src/animation/AnimateDif_sdxl
-CUR_PATH=`pwd`
-python -m scripts.animate --exp_config configs/prompts/5-lora.yaml --H 1024 --W 1024 --L 16 --xformers --output_dir ${CUR_PATH}/../../../sdxl_video/${OUTPUT_DIR}
+  # generate video
+  cd ./src/animation/AnimateDif_sd15
+  CUR_PATH=`pwd`
+  python -m scripts.animate --config configs/prompts/5-lora.yaml --output_dir ${CUR_PATH}/../../../sd15_video/${OUTPUT_DIR}
 
-# remove the temporary lora weight
-rm ./models/DreamBooth_LoRA/lora.safetensors
+  # remove the temporary lora weight
+  rm ./models/DreamBooth_LoRA/lora.safetensors
+
+else 
+  python src/animation/config_generator.py --sd_base ${SD_BASE} --lora_weight_path ${LORA_PATH} --prompt_file_path ${PROMPT_FILE} --step ${STEP} --guidance_scale ${GUIDANCE_SCALE} --lora_alpha ${LORA_ALPHA}
+  mv ./5-lora.yaml src/animation/AnimateDiff_sdxl/configs/prompts/
+
+  # copy lora weight
+  cp ${LORA_PATH} src/animation/AnimateDiff_sdxl/models/DreamBooth_LoRA/lora.safetensors
+
+  # generate video
+  cd ./src/animation/AnimateDif_sdxl
+  CUR_PATH=`pwd`
+  python -m scripts.animate --exp_config configs/prompts/5-lora.yaml --H 1024 --W 1024 --L 16 --xformers --output_dir ${CUR_PATH}/../../../sdxl_video/${OUTPUT_DIR}
+
+  # remove the temporary lora weight
+  rm ./models/DreamBooth_LoRA/lora.safetensors
+fi
 
 echo "==>> Video generation completed!"
--- src/animation/config_generator.py
+++ src/animation/config_generator.py
@@ -4,6 +4,8 @@ import os
 
 def parse_args():
     parser = argparse.ArgumentParser()
+    parser.add_argument('--sd_base', type=str, 
+                        help='sd15 or sdxl', default='sdxl', choices=['sd15', 'sdxl'])
     parser.add_argument('--lora_weight_path', type=str, 
                         help='lora weights path', default='')
     parser.add_argument('--prompt_file_path', type=str,   
@@ -22,14 +24,29 @@ def main(args):
     with open(args.prompt_file_path, "r") as f:
         for prompt in f.readlines():
             prompts.append(prompt.replace("\n", ""))
-    conf_dict = {"lora_path": "models/DreamBooth_LoRA/lora.safetensors",
-                 "motion_module_path": "models/Motion_Module/mm_sdxl_v10_beta.ckpt",
-                 "seed": -1,
-                 "guidance_scale": args.guidance_scale,
-                 "step": args.step,
-                 "lora_alpha": args.lora_alpha, 
-                 "prompt": prompts,
-                 "n_prompt": ["bad quality,worst quality" for _ in range(len(prompts))]}
+    if args.sd_base == "sd15":
+        conf_dict = {"domain_lora_scale": 1.0,
+                     "adapter_lora_path": "models/Motion_Module/v3_sd15_adapter.ckpt",
+                     "lora_path": "models/DreamBooth_LoRA/lora.safetensors",
+                     "inference_config": "configs/inference/inference-v3.yaml",
+                     "motion_module": "models/Motion_Module/v3_sd15_mm.ckpt",
+                     "seed": -1,
+                     "guidance_scale": args.guidance_scale,
+                     "step": args.step,
+                     "lora_alpha": args.lora_alpha, 
+                     "prompt": prompts,
+                     "n_prompt": ["bad quality,worst quality" for _ in range(len(prompts))]}
+        conf_dict = [conf_dict]
+    elif args.sd_base == "sdxl":
+        conf_dict = {"lora_path": "models/DreamBooth_LoRA/lora.safetensors",
+                    "motion_module_path": "models/Motion_Module/mm_sdxl_v10_beta.ckpt",
+                    "seed": -1,
+                    "guidance_scale": args.guidance_scale,
+                    "step": args.step,
+                    "lora_alpha": args.lora_alpha, 
+                    "prompt": prompts,
+                    "n_prompt": ["bad quality,worst quality" for _ in range(len(prompts))]}
+    
     
     conf = OmegaConf.create(conf_dict)
     OmegaConf.save(conf, "./5-lora.yaml")
