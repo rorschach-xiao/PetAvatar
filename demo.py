60406f3fea55daf36bcbf1368f160340556b1e67
yang.xiao01 <yang.xiao01@bytedance.com>
2024-04-17 10:13:06 -0700

    fix conda bugs

--- demo.py
+++ demo.py
@@ -164,7 +164,7 @@ def generate_videos(
     # )
     subprocess.run(
         [
-            "source",
+            ".",
             f"scripts/gen_video_inference.sh",
             lora_path,
             prompt_file,
